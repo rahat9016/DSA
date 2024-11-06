7478f85c8665422b612320e35f91fc34478b1fdc
Minhajur Rohoman <75428759+rahat9016@users.noreply.github.com>
2024-11-06 13:19:35 +0600

    print number end of 1-n

--- Patterns.cpp
+++ Patterns.cpp
@@ -30,4 +30,13 @@ int main() {
     }
     return 0;
 }
-
+//  print number end of 1-n
+int n = 3; 
+    int num = 1;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<num;
+            num++;
+        }
+        cout<<endl;
+    }
