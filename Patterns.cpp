ce67c3f79cf120744c2cbdded74b758bc9a43e63
Minhajur Rohoman <75428759+rahat9016@users.noreply.github.com>
2024-11-06 15:43:37 +0600

    triangle pattern 2

--- Patterns.cpp
+++ Patterns.cpp
@@ -70,3 +70,14 @@ for(int i = 0; i < n; i++){
     }
     cout<<endl;
 }
+// triangle pattern 2
+for(int i = 0; i < n; i++){
+        for(int k =0; k<i; k++ ){
+            cout<<" ";
+        }
+        for(int j = 0; j < n-i; j++){
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
