f516ee0b12fd96634ce0ba15ac6fb524d53d0acb
Minhajur Rohoman <75428759+rahat9016@users.noreply.github.com>
2024-11-07 12:24:10 +0600

    Reverse  Linear Search

--- linearSearchAlgorithom.cpp
+++ linearSearchAlgorithom.cpp
@@ -1,3 +1,4 @@
+// 2. Linear Search 
 #include <iostream>
 using namespace std;
 
@@ -16,5 +17,30 @@ int main() {
     int target_value = linearSearchAlgorithom(arr, size, target);
     cout<<target_value;
     
+    return 0;
+}
+
+// 2. Reverse  Linear Search 
+#include <iostream>
+using namespace std;
+int main() {
+    
+    
+    int arr[] = {1,2,3,4,5};
+    int size = 5;
+    int start =0, end = size - 1;
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+    while(start < end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    cout<<endl;
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+
     return 0;
 }
